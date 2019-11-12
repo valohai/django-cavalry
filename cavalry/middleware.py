d0fe2c493dd34ead2aa3a29c68381d24c8821188
Aarni Koskela <akx@iki.fi>
2019-11-12 08:47:00 +0200

    Move settings bits to policy; only record stacks if we're going to use them

--- cavalry/middleware.py
+++ cavalry/middleware.py
@@ -1,12 +1,11 @@
-from random import random
 from threading import Thread
 
-from django.conf import settings
 from django.db import connections
 from django.template.response import SimpleTemplateResponse
 
 from cavalry.db import force_debug_cursor, patch_db
 from cavalry.locals import managed
+from cavalry.policy import can_cavalrize, can_inject_stats, can_post_stats, can_post_threaded, can_record_stacks
 from cavalry.poster import post_stats
 from cavalry.reporter import inject_stats
 from cavalry.timing import get_time
@@ -14,7 +13,7 @@ from cavalry.timing import get_time
 
 def _process(request, get_response):
     with force_debug_cursor(), managed(
-        db_record_stacks=getattr(settings, 'CAVALRY_DB_RECORD_STACKS', True),
+        db_record_stacks=can_record_stacks(request),
     ) as data:
         data['start_time'] = get_time()
         response = get_response(request)
@@ -30,26 +29,25 @@ def _process(request, get_response):
                 'n_queries': len(queries),
                 'time': (sum(q.get('hrtime', 0) * 1000 for q in queries) if queries else 0),
             }
-    inject_stats(request, response, data)
-    post_stats_kwargs = {'request': request, 'response': response, 'data': data}
 
-    if getattr(settings, 'CAVALRY_THREADED_POST', False):
-        Thread(name='cavalry poster', target=post_stats, kwargs=post_stats_kwargs, daemon=False).start()
-    else:
-        post_stats(**post_stats_kwargs)
+    if can_inject_stats(request):
+        inject_stats(request, response, data)
+
+    if can_post_stats(request):
+        post_stats_kwargs = {'request': request, 'response': response, 'data': data}
+        if can_post_threaded(request):
+            Thread(name='cavalry poster', target=post_stats, kwargs=post_stats_kwargs, daemon=False).start()
+        else:
+            post_stats(**post_stats_kwargs)
+
     return response
-
-
-def should_cavalrize(request):
-    probability = getattr(settings, 'CAVALRY_PROBABILITY', 1.0)
-    return probability >= 1 or (random() < probability)
 
 
 def cavalry(get_response):
     patch_db()
 
     def middleware(request):
-        if not (getattr(settings, 'CAVALRY_ENABLED', False) and should_cavalrize(request)):
+        if not can_cavalrize(request):
             return get_response(request)
         return _process(request, get_response)
 
